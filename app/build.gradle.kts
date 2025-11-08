eb4c765bbb407abf8e5066595f1a505ada29b860
Vitaliy-Golikov <vitala.golikovh@yandex.ru>
2025-11-08 18:36:10 +0300

    Setup Gradle project with App class

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    application
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

781456c6b2f31e7425e185c7b1df2f5f22cd2004
Vitaliy-Golikov <vitala.golikovh@yandex.ru>
2025-11-09 00:19:51 +0300

    12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,11 @@
 plugins {
     application
     jacoco
-    id("com.github.ben-manes.versions") version "0.51.0"
     id("org.sonarqube") version "7.0.1.6134"
 }
 
-sonar {
-    properties {
-        property("sonar.projectKey", "Vitaliy-Golikov_java-project-61")
-        property("sonar.organization", "vitaliy-golikov")
-    }
+repositories {
+    mavenCentral()
 }
 
 java {
@@ -34,10 +30,41 @@ tasks.test {
     useJUnitPlatform()
 }
 
+// УДАЛИТЕ блок jacoco {} - он не нужен
+
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
 
-repositories {
-    mavenCentral()
+sonar {
+    properties {
+        property("sonar.projectKey", "Vitaliy-Golikov_java-project-61")
+        property("sonar.organization", "vitaliy-golikov")
+        property("sonar.projectName", "java-project-61")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.token", System.getenv("SONAR_TOKEN"))
+        property("sonar.sources", "src/main/java")
+        property("sonar.tests", "src/test/java")
+        property("sonar.java.binaries", "build/classes")
+        property("sonar.junit.reportPaths", "build/test-results/test")
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.java.source", "21")
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Vitaliy-Golikov_java-project-61")
+        property("sonar.organization", "vitaliy-golikov")
+        property("sonar.projectName", "java-project-61")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.token", System.getenv("SONAR_TOKEN"))
+        property("sonar.sources", "src/main/java")
+        property("sonar.tests", "src/test/java")
+        property("sonar.java.binaries", "build/classes")
+        property("sonar.junit.reportPaths", "build/test-results/test")
+        // property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml") // ЗАКОММЕНТИРУЙТЕ пока
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.java.source", "21")
+    }
 }
---
+++ app/src/test/java/code/AppTest.java
@@ -0,0 +1,7 @@
+package code;
+
+public class AppTest {
+    public static void main(String[] args) {
+
+    }
+}
